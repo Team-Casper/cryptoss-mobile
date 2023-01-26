cb97410e51578237d35126f205d4cc11f1fb63a3
JunnieLee <heunghari@naver.com>
2023-01-27 01:40:59 +0900

    depth 0 기본 navigation 구조 잡기

---
+++ src/presentation/screens/RootNavigator.tsx
@@ -0,0 +1,57 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {Text, View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function OnboardingNavigator() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'pink',
+      }}>
+      <Text style={{color: 'black'}}>Onboarding Screen</Text>
+    </View>
+  );
+}
+
+function SendNavigator() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Send Screen</Text>
+    </View>
+  );
+}
+
+// TO-DO : user state 에 따라 해당 user가 이미 한번 onboarding을 진행한 상태면 다시 앱에 진입했을때 곧바로 main 화면으로 보내주기
+export const RootNavigator = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}
+          initialRouteName="OnboardingNavigator">
+          <Stack.Screen
+            component={OnboardingNavigator}
+            name="OnboardingNavigator"
+          />
+          <Stack.Screen component={HomeScreen} name="HomeScreen" />
+          <Stack.Screen component={SendNavigator} name="SendNavigator" />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
