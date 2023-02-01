5d5066945b3529cca5b2402f6cd5b667e7401457
thyoondev <th.yoon.dev@gmail.com>
2023-02-01 11:59:13 +0900

    add setup axios and swr

--- App.tsx
+++ App.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import {RootNavigator} from './src/presentation/screens/RootNavigator';
 
 import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {SWRConfig} from 'swr';
+import axios from 'axios';
+import Operator from 'swrapi/Operator';
 
 function App(): JSX.Element {
   return (
     <>
-      <RootNavigator />
+      <SWRConfig
+        value={{
+          fetcher: (url: string) => axios.get(url).then(res => res.data),
+        }}>
+        {/* <Operator /> */}
+        <RootNavigator />
+      </SWRConfig>
     </>
   );
 
